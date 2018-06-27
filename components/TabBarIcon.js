4c28fc0f7f138b2fb7cc75183f13f81fb30b6852
Lasse Norfeldt <lasse@norfeldt.dk>
2018-06-27 12:17:35 +0200

    Custom Icons and text in TabBar

--- components/TabBarIcon.js
+++ components/TabBarIcon.js
@@ -1,17 +1,23 @@
-import React from 'react';
-import { Icon } from 'expo';
+import React from 'react'
+//import { Icon } from 'expo'
 
-import Colors from '../constants/Colors';
+import Colors from '../constants/Colors'
+
+import { createIconSetFromFontello } from '@expo/vector-icons'
+import fontelloConfig from '../assets/fonts/config.json'
+const Icon = createIconSetFromFontello(fontelloConfig, 'q180624')
 
 export default class TabBarIcon extends React.Component {
   render() {
     return (
-      <Icon.Ionicons
+      <Icon
         name={this.props.name}
         size={26}
         style={{ marginBottom: -3 }}
-        color={this.props.focused ? Colors.tabIconSelected : Colors.tabIconDefault}
+        color={
+          this.props.focused ? Colors.tabIconSelected : Colors.tabIconDefault
+        }
       />
-    );
+    )
   }
 }
