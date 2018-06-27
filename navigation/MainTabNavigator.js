ee80cf83bccb9aba915b35fae03a26bcf47a4504
Lasse Norfeldt <lasse@norfeldt.dk>
2018-06-27 11:16:44 +0200

    .gitignore is now working

--- navigation/MainTabNavigator.js
+++ navigation/MainTabNavigator.js
@@ -1,15 +1,18 @@
-import React from 'react';
-import { Platform } from 'react-native';
-import { createStackNavigator, createBottomTabNavigator } from 'react-navigation';
+import React from 'react'
+import { Platform, Image } from 'react-native'
+import {
+  createStackNavigator,
+  createBottomTabNavigator,
+} from 'react-navigation'
 
-import TabBarIcon from '../components/TabBarIcon';
-import HomeScreen from '../screens/HomeScreen';
-import LinksScreen from '../screens/LinksScreen';
-import SettingsScreen from '../screens/SettingsScreen';
+import TabBarIcon from '../components/TabBarIcon'
+import HomeScreen from '../screens/HomeScreen'
+import LinksScreen from '../screens/LinksScreen'
+import SettingsScreen from '../screens/SettingsScreen'
 
 const HomeStack = createStackNavigator({
   Home: HomeScreen,
-});
+})
 
 HomeStack.navigationOptions = {
   tabBarLabel: 'Home',
@@ -23,38 +26,46 @@ HomeStack.navigationOptions = {
       }
     />
   ),
-};
+}
 
 const LinksStack = createStackNavigator({
   Links: LinksScreen,
-});
+})
 
 LinksStack.navigationOptions = {
   tabBarLabel: 'Links',
   tabBarIcon: ({ focused }) => (
     <TabBarIcon
       focused={focused}
-      name={Platform.OS === 'ios' ? `ios-link${focused ? '' : '-outline'}` : 'md-link'}
+      name={
+        Platform.OS === 'ios'
+          ? `ios-link${focused ? '' : '-outline'}`
+          : 'md-link'
+      }
     />
   ),
-};
+}
 
 const SettingsStack = createStackNavigator({
   Settings: SettingsScreen,
-});
+})
 
 SettingsStack.navigationOptions = {
   tabBarLabel: 'Settings',
   tabBarIcon: ({ focused }) => (
     <TabBarIcon
       focused={focused}
-      name={Platform.OS === 'ios' ? `ios-options${focused ? '' : '-outline'}` : 'md-options'}
+      name={
+        Platform.OS === 'ios'
+          ? `ios-options${focused ? '' : '-outline'}`
+          : 'md-options'
+      }
     />
   ),
-};
+}
 
 export default createBottomTabNavigator({
   HomeStack,
   LinksStack,
   SettingsStack,
-});
+})
