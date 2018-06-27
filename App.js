d8b1413ca065a3e2572f8aa10f116d9e9cc3e593
Lasse Norfeldt <lasse@norfeldt.dk>
2018-06-27 12:17:35 +0200

    Custom Icons and text in TabBar

--- App.js
+++ App.js
@@ -1,12 +1,12 @@
-import React from 'react';
-import { Platform, StatusBar, StyleSheet, View } from 'react-native';
-import { AppLoading, Asset, Font, Icon } from 'expo';
-import AppNavigator from './navigation/AppNavigator';
+import React from 'react'
+import { Platform, StatusBar, StyleSheet, View } from 'react-native'
+import { AppLoading, Asset, Font, Icon } from 'expo'
+import AppNavigator from './navigation/AppNavigator'
 
 export default class App extends React.Component {
   state = {
     isLoadingComplete: false,
-  };
+  }
 
   render() {
     if (!this.state.isLoadingComplete && !this.props.skipLoadingScreen) {
@@ -16,14 +16,14 @@ export default class App extends React.Component {
           onError={this._handleLoadingError}
           onFinish={this._handleFinishLoading}
         />
-      );
+      )
     } else {
       return (
         <View style={styles.container}>
           {Platform.OS === 'ios' && <StatusBar barStyle="default" />}
           <AppNavigator />
         </View>
-      );
+      )
     }
   }
 
@@ -39,19 +39,20 @@ export default class App extends React.Component {
         // We include SpaceMono because we use it in HomeScreen.js. Feel free
         // to remove this if you are not using it in your app
         'space-mono': require('./assets/fonts/SpaceMono-Regular.ttf'),
+        q180624: require('./assets/fonts/q180624.ttf'),
       }),
-    ]);
-  };
+    ])
+  }
 
   _handleLoadingError = error => {
     // In this case, you might want to report the error to your error
     // reporting service, for example Sentry
-    console.warn(error);
-  };
+    console.warn(error)
+  }
 
   _handleFinishLoading = () => {
-    this.setState({ isLoadingComplete: true });
-  };
+    this.setState({ isLoadingComplete: true })
+  }
 }
 
 const styles = StyleSheet.create({
@@ -59,4 +60,4 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
   },
-});
+})
--- navigation/MainTabNavigator.js
+++ navigation/MainTabNavigator.js
@@ -1,60 +1,60 @@
-import React from 'react';
-import { Platform } from 'react-native';
-import { createStackNavigator, createBottomTabNavigator } from 'react-navigation';
+import React from 'react'
+import { Platform } from 'react-native'
+import {
+  createStackNavigator,
+  createBottomTabNavigator,
+} from 'react-navigation'
 
-import TabBarIcon from '../components/TabBarIcon';
-import HomeScreen from '../screens/HomeScreen';
-import LinksScreen from '../screens/LinksScreen';
-import SettingsScreen from '../screens/SettingsScreen';
+import TabBarIcon from '../components/TabBarIcon'
+import HomeScreen from '../screens/HomeScreen'
+import LinksScreen from '../screens/LinksScreen'
+import SettingsScreen from '../screens/SettingsScreen'
 
-const HomeStack = createStackNavigator({
-  Home: HomeScreen,
-});
+const DiagnosticStack = createStackNavigator({
+  diagnostic: HomeScreen,
+})
 
-HomeStack.navigationOptions = {
-  tabBarLabel: 'Home',
+DiagnosticStack.navigationOptions = {
+  tabBarLabel: 'Diagnostic',
   tabBarIcon: ({ focused }) => (
-    <TabBarIcon
-      focused={focused}
-      name={
-        Platform.OS === 'ios'
-          ? `ios-information-circle${focused ? '' : '-outline'}`
-          : 'md-information-circle'
-      }
-    />
+    <TabBarIcon focused={focused} name="diagnostic" />
   ),
-};
+}
 
-const LinksStack = createStackNavigator({
-  Links: LinksScreen,
-});
+const ManufactureStack = createStackNavigator({
+  manufacture: LinksScreen,
+})
 
-LinksStack.navigationOptions = {
-  tabBarLabel: 'Links',
+ManufactureStack.navigationOptions = {
+  tabBarLabel: 'Manufacture',
   tabBarIcon: ({ focused }) => (
-    <TabBarIcon
-      focused={focused}
-      name={Platform.OS === 'ios' ? `ios-link${focused ? '' : '-outline'}` : 'md-link'}
-    />
+    <TabBarIcon focused={focused} name="manufacture" />
   ),
-};
+}
 
-const SettingsStack = createStackNavigator({
-  Settings: SettingsScreen,
-});
+const PatientStack = createStackNavigator({
+  Patient: SettingsScreen,
+})
 
-SettingsStack.navigationOptions = {
-  tabBarLabel: 'Settings',
+PatientStack.navigationOptions = {
+  tabBarLabel: 'Patient',
+  tabBarIcon: ({ focused }) => <TabBarIcon focused={focused} name="patient" />,
+}
+
+const SerializationStack = createStackNavigator({
+  Serialization: HomeScreen,
+})
+
+SerializationStack.navigationOptions = {
+  tabBarLabel: 'Serialization',
   tabBarIcon: ({ focused }) => (
-    <TabBarIcon
-      focused={focused}
-      name={Platform.OS === 'ios' ? `ios-options${focused ? '' : '-outline'}` : 'md-options'}
-    />
+    <TabBarIcon focused={focused} name="block_chain" />
   ),
-};
+}
 
 export default createBottomTabNavigator({
-  HomeStack,
-  LinksStack,
-  SettingsStack,
-});
+  DiagnosticStack,
+  ManufactureStack,
+  PatientStack,
+  SerializationStack,
+})
