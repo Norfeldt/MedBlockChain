6a90a7ba11657806bd68bee32510be092605c2a1
Lasse Norfeldt <lasse@norfeldt.dk>
2018-07-04 10:18:53 +0200

    converted to stateless component

--- components/FontIcon.js
+++ components/FontIcon.js
@@ -4,14 +4,6 @@ import { createIconSetFromFontello } from '@expo/vector-icons'
 import fontelloConfig from '../assets/fonts/config.json'
 const Icon = createIconSetFromFontello(fontelloConfig, 'q180624')
 
-export default class FontIcon extends React.Component {
-  render() {
-    return (
-      <Icon
-        name={this.props.name}
-        size={this.props.size}
-        color={this.props.color}
-      />
-    )
-  }
-}
+export default (Button = ({ name, size, color, style }) => {
+  return <Icon name={name} size={size} color={color} style={style} />
+})
