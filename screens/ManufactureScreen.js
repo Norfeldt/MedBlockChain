5bdfdc5d5db0ef7d53cc7fa1e24fc6cedf1e4148
Lasse Norfeldt <lasse@norfeldt.dk>
2018-07-08 08:03:34 +0200

    fix lag issue with sliders

--- screens/ManufactureScreen.js
+++ screens/ManufactureScreen.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { StyleSheet, ScrollView, Slider } from 'react-native'
 import QRCode from 'react-native-qrcode'
 import { SHA256 } from 'crypto-js'
+import { debounce } from 'lodash'
 
 import Header from '../components/Header'
 import InfoRow from '../components/InfoRow'
@@ -14,14 +15,47 @@ export default class HomeScreen extends React.Component {
     header: <Header title="Manufacture" />,
   }
 
-  state = {
-    MANUFACTURE_NAME: 'Jukka Labs',
-    PRODUCTION_DATE: '24 June 2018',
-    DRUG_A_VALUE: 4.25,
-    DRUG_A_UNITS: 'mg',
-    DRUG_B_VALUE: 80,
-    DRUG_B_UNITS: 'mg',
-    HASH_SALT: 'HAWQ',
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      MANUFACTURE_NAME: 'Jukka Labs',
+      PRODUCTION_DATE: '24 June 2018',
+      DRUG_A_VALUE: 4.25,
+      DRUG_A_UNITS: 'mg',
+      DRUG_B_VALUE: 80,
+      DRUG_B_UNITS: 'mg',
+      HASH_SALT: 'HAWQ',
+      QR: 'loading',
+      hash: 'loading',
+    }
+
+    this.debounceDoseA = debounce(this.updateDoseA, 25)
+    this.debounceDoseB = debounce(this.updateDoseB, 25)
+    this.debounceUpdate = debounce(this.updateQRAndHash, 500)
+  }
+
+  updateDoseA(DRUG_A_VALUE) {
+    this.setState({ DRUG_A_VALUE })
+    this.debounceUpdate()
+  }
+
+  updateDoseB(DRUG_B_VALUE) {
+    this.setState({ DRUG_B_VALUE })
+    this.debounceUpdate()
+  }
+
+  updateQRAndHash() {
+    const { QR, hash, ...cleanedState } = this.state
+    const stateStr = JSON.stringify(cleanedState)
+    this.setState({
+      QR: stateStr,
+      hash: SHA256(stateStr).toString(),
+    })
+  }
+
+  componentDidMount() {
+    this.updateQRAndHash()
   }
 
   render() {
@@ -33,6 +67,8 @@ export default class HomeScreen extends React.Component {
       DRUG_B_VALUE,
       DRUG_B_UNITS,
       HASH_SALT,
+      QR,
+      hash,
     } = this.state
 
     return (
@@ -57,7 +93,7 @@ export default class HomeScreen extends React.Component {
           maximumValue={20}
           value={DRUG_A_VALUE}
           step={0.25}
-          onValueChange={DRUG_A_VALUE => this.setState({ DRUG_A_VALUE })}
+          onValueChange={value => this.debounceDoseA(value)}
         />
 
         <InfoRow
@@ -70,13 +106,13 @@ export default class HomeScreen extends React.Component {
           maximumValue={200}
           value={DRUG_B_VALUE}
           step={10}
-          onValueChange={DRUG_B_VALUE => this.setState({ DRUG_B_VALUE })}
+          onValueChange={value => this.debounceDoseB(value)}
         />
 
         <InfoRow setting="Hash Salt" value={HASH_SALT} />
 
         <QRCode
-          value={JSON.stringify({ ...this.state })}
+          value={QR}
           size={150}
           bgColor={Colors.tintColor}
           fgColor="white"
@@ -84,10 +120,9 @@ export default class HomeScreen extends React.Component {
 
         <InfoRow
           setting="Serialize ID (SHA256)"
-          value={`${SHA256(JSON.stringify({ ...this.state }))
-            .toString()
-            .slice(0, 10)}...`}
+          value={`${hash.slice(0, 10)}...`}
         />
+
         <Button title="CHECK IN" iconName="check_in" />
       </ScrollView>
     )
