58329971f261a3d11f7d16f02a81604a7b5148e4
Lasse Norfeldt <lasse@norfeldt.dk>
2018-08-26 21:52:16 +0200

    New slider

--- components/DoseAdjuster.js
+++ components/DoseAdjuster.js
@@ -1,5 +1,7 @@
 import React, { PureComponent } from 'react'
-import { Slider } from 'react-native'
+import { View } from 'react-native'
+import Text from './basic/Text'
+import Slider from 'react-native-slider'
 
 import debounce from 'lodash/debounce'
 
@@ -12,16 +14,27 @@ export default class DoseAdjuster extends PureComponent {
     return (
       <ContextConsumer>
         {context => {
+          const { minDose, maxDose } = context.getDoseRange()
           return (
-            <Slider
-              style={{ height: 60, alignSelf: 'stretch' }}
-              minimumTrackTintColor={Colors.tintColor}
-              minimumValue={context.getDoseRange().minDose}
-              maximumValue={context.getDoseRange().maxDose}
-              value={context.getDose()}
-              step={0.25}
-              onValueChange={debounce(value => context.setDose(value), 50)}
-            />
+            <View
+              style={{ flex: 1, flexDirection: 'row', alignItems: 'center' }}
+            >
+              <Text style={{ flex: 15, fontSize: 12 }}>
+                {minDose.toFixed(2)}
+              </Text>
+              <Slider
+                style={{ flex: 70, height: 60, alignSelf: 'stretch' }}
+                minimumTrackTintColor={Colors.tintColor}
+                minimumValue={minDose}
+                maximumValue={maxDose}
+                value={context.getDose()}
+                step={0.25}
+                onValueChange={debounce(value => context.setDose(value), 50)}
+              />
+              <Text style={{ flex: 15, fontSize: 12 }}>
+                {maxDose.toFixed(2)}
+              </Text>
+            </View>
           )
         }}
       </ContextConsumer>
