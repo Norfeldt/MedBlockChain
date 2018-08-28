5d3280eb71c7a7b09a7ed4268f5a3576b5cbfbc1
Lasse Norfeldt <lasse@norfeldt.dk>
2018-08-28 14:45:50 +0200

    possible to use FontAwesome icons

--- components/basic/FontIcon.js
+++ components/basic/FontIcon.js
@@ -1,12 +1,16 @@
 import React, { PureComponent } from 'react'
-
+import { FontAwesome } from '@expo/vector-icons'
 import { createIconSetFromFontello } from '@expo/vector-icons'
 import fontelloConfig from '../../assets/fonts/config.json'
 const Icon = createIconSetFromFontello(fontelloConfig, 'q180624')
 
 export default class FontIcon extends PureComponent {
   render() {
-    const { style, name, size, color } = this.props
+    const { style, name, size, color, font } = this.props
+
+    if (font && font.toLowerCase() == 'fontawesome') {
+      return <FontAwesome style={style} name={name} size={size} color={color} />
+    }
     return <Icon style={style} name={name} size={size} color={color} />
   }
 }
